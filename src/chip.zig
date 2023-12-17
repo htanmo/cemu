0f20a4d0c7c5d02234ca5775dc17c4a07a455583
Omnath Mandal <145841395+htanmo@users.noreply.github.com>
2023-12-17 14:25:28 +0530

    Added Source files
    [Note: Zig v0.11.0]
    
    - build.zig
    - chip.zig
    - main.zig

---
+++ src/chip.zig
@@ -0,0 +1,309 @@
+const std = @import("std");
+const cstd = @cImport(@cInclude("stdlib.h"));
+const time = @cImport(@cInclude("time.h"));
+
+opcode: u16,
+memory: [4096]u8,
+graphics: [64 * 32]u8,
+registers: [16]u8,
+index: u16,
+program_counter: u16,
+
+delay_timer: u8,
+sound_timer: u8,
+
+stack: [16]u16,
+sp: u16,
+
+keys: [16]u8,
+
+// zig fmt: off
+const chip8_fontset = [_]u8
+{
+    0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+    0x20, 0x60, 0x20, 0x20, 0x70, // 1
+    0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+    0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+    0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+    0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+    0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+    0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+    0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+    0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+    0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+    0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+    0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+    0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+    0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+    0xF0, 0x80, 0xF0, 0x80, 0x80  // F
+};
+// zig fmt: on
+
+const Self = @This();
+
+pub fn init(self: *Self) void {
+    cstd.srand(@intCast(time.time(0))); // srand(time(NULL));
+
+    self.program_counter = 0x200;
+    self.opcode = 0;
+    self.index = 0;
+    self.sp = 0;
+    self.delay_timer = 0;
+    self.sound_timer = 0;
+
+    for (&self.graphics) |*x| {
+        x.* = 0;
+    }
+    for (&self.memory) |*x| {
+        x.* = 0;
+    }
+    for (&self.stack) |*x| {
+        x.* = 0;
+    }
+    for (&self.registers) |*x| {
+        x.* = 0;
+    }
+    for (&self.keys) |*x| {
+        x.* = 0;
+    }
+    for (chip8_fontset, 0..) |c, idx| {
+        self.memory[idx] = c;
+    }
+}
+
+fn increment_pc(self: *Self) void {
+    self.program_counter += 2;
+}
+
+pub fn cycle(self: *Self) void {
+    self.opcode = @as(u16, @intCast(self.memory[self.program_counter])) << 8 | self.memory[self.program_counter + 1];
+
+    //X000
+    var first = self.opcode >> 12;
+
+    switch (first) {
+        0x0 => {
+            if (self.opcode == 0x00E0) {
+                for (&self.graphics) |*pixel| {
+                    pixel.* = 0;
+                }
+            } else if (self.opcode == 0x00EE) {
+                self.program_counter = self.stack[self.sp];
+                self.sp -= 1;
+            }
+            self.increment_pc();
+        },
+        0x1 => {
+            self.program_counter = self.opcode & 0x0FFF;
+        },
+        0x2 => {
+            self.stack[self.sp] = self.program_counter;
+            self.sp += 1;
+            self.program_counter = self.opcode & 0x0FFF;
+        },
+        0x3 => {
+            var x = (self.opcode & 0x0F00) >> 8;
+
+            if (self.registers[x] == self.opcode & 0x00FF) {
+                self.increment_pc();
+            }
+            self.increment_pc();
+        },
+        0x4 => {
+            var x = (self.opcode & 0x0F00) >> 8;
+
+            if (self.registers[x] != self.opcode & 0x00FF) {
+                self.increment_pc();
+            }
+            self.increment_pc();
+        },
+        0x5 => {
+            var x = (self.opcode & 0x0F00) >> 8;
+            var y = (self.opcode & 0x00F0) >> 4;
+
+            if (self.registers[x] == self.registers[y]) {
+                self.increment_pc();
+            }
+            self.increment_pc();
+        },
+        0x6 => {
+            var x = (self.opcode & 0x0F00) >> 8;
+            self.registers[x] = @truncate(self.opcode & 0x00FF);
+            self.increment_pc();
+        },
+        0x7 => {
+            @setRuntimeSafety(false);
+            var x = (self.opcode & 0x0F00) >> 8;
+            self.registers[x] += @truncate(self.opcode & 0x00FF);
+            self.increment_pc();
+        },
+        0x8 => {
+            var x = (self.opcode & 0x0F00) >> 8;
+            var y = (self.opcode & 0x00F0) >> 4;
+            var m = self.opcode & 0x000F;
+
+            switch (m) {
+                0 => self.registers[x] = self.registers[y],
+                1 => self.registers[x] |= self.registers[y],
+                2 => self.registers[x] &= self.registers[y],
+                3 => self.registers[x] ^= self.registers[y],
+                4 => {
+                    @setRuntimeSafety(false);
+                    var sum: u16 = self.registers[x];
+                    sum += self.registers[y];
+
+                    self.registers[0xF] = if (sum > 255) 1 else 0;
+                    self.registers[x] = @truncate(sum & 0x00FF);
+                },
+                5 => {
+                    @setRuntimeSafety(false);
+                    self.registers[0xF] = if (self.registers[x] > self.registers[y]) 1 else 0;
+                    self.registers[x] = self.registers[y] - self.registers[x];
+                },
+                6 => {
+                    self.registers[0xF] = self.registers[x] & 1;
+                    self.registers[x] >>= 1;
+                },
+                7 => {
+                    @setRuntimeSafety(false);
+                    self.registers[0xF] = if (self.registers[y] > self.registers[x]) 1 else 0;
+                    self.registers[x] = self.registers[y] - self.registers[x];
+                },
+                14 => {
+                    self.registers[0xF] = if (self.registers[x] & 0x80 != 0) 1 else 0;
+                    self.registers[x] <<= 1;
+                },
+                else => {},
+            }
+
+            self.increment_pc();
+        },
+        0x9 => {
+            var x = (self.opcode & 0x0F00) >> 8;
+            var y = (self.opcode & 0x00F0) >> 4;
+
+            if (self.registers[x] != self.registers[y]) {
+                self.increment_pc();
+            }
+            self.increment_pc();
+        },
+        0xA => {
+            self.index = self.opcode & 0x0FFF;
+            self.increment_pc();
+        },
+        0xB => {
+            self.program_counter = (self.opcode & 0x0FFF) + @as(u16, @intCast(self.registers[0]));
+        },
+        0xC => {
+            var x = (self.opcode & 0x0F00) >> 8;
+            var kk = self.opcode & 0x00FF;
+
+            self.registers[x] = @truncate(@as(u8, @intCast(cstd.rand())) & kk);
+            self.increment_pc();
+        },
+        0xD => {
+            self.registers[0xF] = 0;
+
+            var xx = (self.opcode & 0x0F00) >> 8;
+            var yy = (self.opcode & 0x00F0) >> 4;
+            var nn = self.opcode & 0x000F;
+
+            var regX = self.registers[xx];
+            var regY = self.registers[yy];
+
+            var y: usize = 0;
+            while (y < nn) : (y += 1) {
+                var pixel = self.memory[self.index + y];
+
+                var x: usize = 0;
+                while (x < 8) : (x += 1) {
+                    const msb: u8 = 0x80;
+
+                    // 0b01010101
+                    if (pixel & (msb >> @intCast(x)) != 0) {
+                        var tX = (regX + x) % 64;
+                        var tY = (regY + y) % 32;
+
+                        var idx = tX + tY * 64;
+
+                        self.graphics[idx] ^= 1;
+
+                        if (self.graphics[idx] == 0) {
+                            self.registers[0xF] = 1;
+                        }
+                    }
+                }
+            }
+            self.increment_pc();
+        },
+        0xE => {
+            var x = (self.opcode & 0x0F00) >> 8;
+            var kk = self.opcode & 0x00FF;
+
+            if (kk == 0x9E) {
+                if (self.keys[self.registers[x]] == 1) {
+                    self.increment_pc();
+                }
+            } else if (kk == 0xA1) {
+                if (self.keys[self.registers[x]] != 1) {
+                    self.increment_pc();
+                }
+            }
+            self.increment_pc();
+        },
+        0xF => {
+            var x = (self.opcode & 0x0F00) >> 8;
+            var kk = self.opcode & 0x00FF;
+
+            if (kk == 0x07) {
+                self.registers[x] = self.delay_timer;
+            } else if (kk == 0x0A) {
+                var key_pressed = false;
+
+                for (self.keys, 0..) |v, idx| {
+                    if (v != 0) {
+                        self.registers[x] = @truncate(idx);
+                        key_pressed = true;
+                        break;
+                    }
+                }
+
+                if (!key_pressed)
+                    return;
+            } else if (kk == 0x15) {
+                self.delay_timer = self.registers[x];
+            } else if (kk == 0x18) {
+                self.sound_timer = self.registers[x];
+            } else if (kk == 0x1E) {
+                self.index += self.registers[x];
+            } else if (kk == 0x29) {
+                if (self.registers[x] < 16) {
+                    self.index = self.registers[x] * 0x5;
+                }
+            } else if (kk == 0x33) {
+                self.memory[self.index] = self.registers[x] / 100;
+                self.memory[self.index + 1] = (self.registers[x] / 10) % 10;
+                self.memory[self.index + 2] = self.registers[x] % 10;
+            } else if (kk == 0x55) {
+                var i: usize = 0;
+                while (i <= x) : (i += 1) {
+                    self.memory[self.index + i] = self.registers[i];
+                }
+            } else if (kk == 0x65) {
+                var i: usize = 0;
+                while (i <= x) : (i += 1) {
+                    self.registers[i] = self.memory[self.index + i];
+                }
+            }
+            self.increment_pc();
+        },
+        else => {},
+    }
+
+    if (self.delay_timer > 0) {
+        self.delay_timer -= 1;
+    }
+    if (self.sound_timer > 0) {
+        self.sound_timer -= 1;
+    }
+}
---
+++ src/main.zig
@@ -0,0 +1,152 @@
+const std = @import("std");
+const sdl = @cImport({
+    @cInclude("SDL.h");
+});
+const CHIP8 = @import("chip.zig");
+const process = std.process;
+
+var window: ?*sdl.SDL_Window = null;
+var renderer: ?*sdl.SDL_Renderer = null;
+var texture: ?*sdl.SDL_Texture = null;
+
+var cpu: *CHIP8 = undefined;
+
+const keymap: [16]c_int = [_]c_int{
+    sdl.SDL_SCANCODE_X,
+    sdl.SDL_SCANCODE_1,
+    sdl.SDL_SCANCODE_2,
+    sdl.SDL_SCANCODE_3,
+    sdl.SDL_SCANCODE_Q,
+    sdl.SDL_SCANCODE_W,
+    sdl.SDL_SCANCODE_E,
+    sdl.SDL_SCANCODE_A,
+    sdl.SDL_SCANCODE_S,
+    sdl.SDL_SCANCODE_D,
+    sdl.SDL_SCANCODE_Z,
+    sdl.SDL_SCANCODE_C,
+    sdl.SDL_SCANCODE_4,
+    sdl.SDL_SCANCODE_R,
+    sdl.SDL_SCANCODE_F,
+    sdl.SDL_SCANCODE_V,
+};
+
+pub fn init() !void {
+    // zig fmt: off
+    if (sdl.SDL_Init(sdl.SDL_INIT_VIDEO) < 0)
+        @panic("SDL Initialization Failed!");
+
+    window = sdl.SDL_CreateWindow(
+        "CHIP8",
+        sdl.SDL_WINDOWPOS_CENTERED,
+        sdl.SDL_WINDOWPOS_CENTERED,
+        1024,
+        512,
+        0
+    );
+
+    renderer = sdl.SDL_CreateRenderer(window, -1, 0);
+
+    if (renderer == null)
+        @panic("SDL Renderer Initialization Failed!");
+
+    texture = sdl.SDL_CreateTexture(
+        renderer,
+        sdl.SDL_PIXELFORMAT_RGBX8888,
+        sdl.SDL_TEXTUREACCESS_STREAMING,
+        64,
+        32
+    );
+
+    if (texture == null)
+        @panic("SDL Texture Creation Failed!");
+    // zig fmt: on
+}
+
+pub fn deinit() void {
+    sdl.SDL_DestroyWindow(window);
+    sdl.SDL_Quit();
+}
+
+pub fn loadROM(filename: []const u8) !void {
+    var inputFile = try std.fs.cwd().openFile(filename, .{});
+    defer inputFile.close();
+
+    var size = try inputFile.getEndPos();
+    var reader = inputFile.reader();
+
+    var i: usize = 0;
+    while (i < size) : (i += 1) {
+        cpu.memory[i + 0x200] = try reader.readByte();
+    }
+}
+
+pub fn main() !void {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+
+    const allocator = arena.allocator();
+
+    try init();
+    defer deinit();
+
+    cpu = try allocator.create(CHIP8);
+    cpu.init();
+
+    var args = try process.argsWithAllocator(allocator);
+    _ = args.skip();
+
+    var filename = args.next() orelse {
+        std.debug.print("No ROM given!", .{});
+        return;
+    };
+
+    try loadROM(filename);
+
+    var running = true;
+    while (running) {
+        cpu.cycle();
+
+        var e: sdl.SDL_Event = undefined;
+        while (sdl.SDL_PollEvent(&e) > 0) {
+            switch (e.type) {
+                sdl.SDL_QUIT => running = false,
+                sdl.SDL_KEYDOWN => {
+                    var i: usize = 0;
+                    while (i < 16) : (i += 1) {
+                        if (e.key.keysym.scancode == keymap[i]) {
+                            cpu.keys[i] = 1;
+                        }
+                    }
+                },
+                sdl.SDL_KEYUP => {
+                    var i: usize = 0;
+                    while (i < 16) : (i += 1) {
+                        if (e.key.keysym.scancode == keymap[i]) {
+                            cpu.keys[i] = 0;
+                        }
+                    }
+                },
+                else => {},
+            }
+        }
+
+        _ = sdl.SDL_RenderClear(renderer);
+
+        var bytes: ?[*]u32 = null;
+        var pitch: c_int = 0;
+
+        // _ = sdl.SDL_LockTexture(texture, null, @ptrCast([*c]?*anyopaque, &bytes), &pitch);
+        _ = sdl.SDL_LockTexture(texture, null, @ptrCast(&bytes), &pitch);
+
+        for (cpu.graphics, 0..) |g, idx| {
+            bytes.?[idx] = if (g == 1) 0xFFFFFFFF else 0x000000FF;
+        }
+
+        sdl.SDL_UnlockTexture(texture);
+
+        _ = sdl.SDL_RenderCopy(renderer, texture, null, null);
+        _ = sdl.SDL_RenderPresent(renderer);
+
+        std.time.sleep(16 * 1000 * 1000);
+    }
+}
